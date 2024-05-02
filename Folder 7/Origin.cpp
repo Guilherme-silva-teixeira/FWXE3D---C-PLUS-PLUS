c0621d46c1da935d99f9ea556f77562a843002c1
DellyPlays <132455461+Guilherme-silva-teixeira@users.noreply.github.com>
2024-05-02 12:19:58 -0300

    Update Origin.cpp

--- Folder 7/Origin.cpp
+++ Folder 7/Origin.cpp
@@ -3,18 +3,30 @@
 
 using namespace std;
 
-class arrayManipulator
+//classe
+class plane
 {
-private:
-	int* array;
-	int size;
-
-public:
-	//constructor dos array aleatória
-	arrayManipulator(int arr[] , int n)
-	{
-		size = n;
-		array = new int[size];
-
-	}
+	public:
+		//elementos publicos 
+		//acessiveis dentro daquela classe e aos objetos
+		int speed = 0;
+		string model = "Cessna 172";
+	private:
+		//elementos privados são acessiveis a apenas aquela classe
 };
+
+int main()
+{
+	//vai definir a linguagem do arquivo
+	setlocale(LC_ALL, "portuguese");
+	//os objetos são instâncias das classes como por exemplo o ponteiro abaixo
+	plane *airplane = new plane(); //objeto
+	//os objetos devem ser declarados como ponteiro
+	std::cout << airplane->speed << std::endl;
+	std::cout << airplane->model << std::endl;
+	//ele vai printar a variáel que está dentro daquela classe porém inicialmente como zero
+	std::cout << "atualize a velocidade do avião" << std::endl;
+	std::cin >> airplane->speed;
+	std::cout << "Á velocidade atual é de: " << airplane->speed << " nós." << std::endl;
+	return 0;
+}
