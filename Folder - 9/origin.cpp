858d165493bde5dd1cb3a264151f7897b07c9744
DellyPlays <132455461+Guilherme-silva-teixeira@users.noreply.github.com>
2024-05-05 15:54:52 -0300

    Update origin.cpp

--- Folder - 9/origin.cpp
+++ Folder - 9/origin.cpp
@@ -28,8 +28,24 @@ class classe01
 
 int main(void)
 {
+	setlocale(LC_ALL, "portuguese");
 	int n0 = 0, n1 = 3;
 	classe01* classe = new classe01();
 	classe->testeif_else01();
+inicio:
+	int v0, v1, res;
+	std::cout << "Digite uma nota: " << std::endl;
+	std::cin >> v0;
+	std::cout << "Digite a segunda nota: " << std::endl;
+	std::cin >> v1;
+	res = ((v0 + v1) / 2);
+	std::cout << "a média das notas é de: " << res << "." << std::endl;
+	std::cout << "Digite uma opção se você quer continuar [s/n]: " << std::endl;
+	string op;
+	std::cin >> op;
+	if (op == "s" or op == "S")
+	{
+		goto inicio;
+	}
 	return 0;
 }
