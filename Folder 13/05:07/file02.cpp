7c5aa1d7f5c7b3400deb1addd2722f6e33ee48fa
DellyPlays <132455461+Guilherme-silva-teixeira@users.noreply.github.com>
2024-05-07 15:55:28 -0300

    Update file02.cpp

--- Folder 13/05:07/file02.cpp
+++ Folder 13/05:07/file02.cpp
@@ -1,1 +1,45 @@
+#include <iostream>
+#include <locale>
 
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "portuguese");
+	int val;
+	std::cout << "Selecione um transporte:\n";
+	std::cout << "[1] carro/moto, [2] avião , [3] helicoptero\n";
+	std::cin >> val;
+	switch (val)
+	{
+	case 1:
+		std::cout << "Transporte terrestre\n";
+		switch (val)
+		{
+		case 1:
+			std::cout << "Carro/moto selecionado" << std::endl;
+			break;
+		}
+	case 2:
+		std::cout << "transporte aéreo" << std::endl;
+		switch (val)
+		{
+		case 2:
+			std::cout << "avião selecionado" << std::endl;
+			break;
+		}
+	case 3:
+		std::cout << "transporte aéreo";
+		switch (val)
+		{
+		case 3:
+			std::cout << "Helicóptero selecionado";
+			break;
+		}
+	default:
+		std::cout << "opção inválida";
+		break;
+	}
+	std::cout << "Fim do programa!";
+	return 0;
+}
